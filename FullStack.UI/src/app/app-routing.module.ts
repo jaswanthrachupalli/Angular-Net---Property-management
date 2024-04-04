796dc7e399f9f839b96102d305e24914b91d162d
jaswanthrachupalli <rachupallijaswanth@gmail.com>
2024-04-03 20:07:03 -0400

    project files after gitignore

---
+++ FullStack.UI/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+const routes: Routes = [
+ { path: 'add-property', component: AddPropertyComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FullStack.UI/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+  registerationForm: FormGroup = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl(null, Validators.required),
+    mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+  }, { validators: this.passwordMatchingValidator });
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.registerationForm);
+  }
+
+  private passwordMatchingValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    if (!password || !confirmPassword) {
+      return null;
+    }
+    return password.value === confirmPassword.value ? null : { notmatched: true };
+  }
+
+}
