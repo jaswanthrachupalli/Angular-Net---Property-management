5ad91a8b6b87fef3eac1f2bc853146af798e0258
jaswanthrachupalli <rachupallijaswanth@gmail.com>
2024-04-22 10:49:36 -0400

    Ensure bin and obj directories are ignored

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -7,7 +7,6 @@ using WebAPI.Data;
 using WebAPI.Data.Repo;
 using WebAPI.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI.Controllers
 {
